6313d956153670ac8317427394004c313ff182c4
Ewa Kadziolka <kalistaewa@gmail.com>
2022-08-04 18:36:58 +0200

    adds color change

--- main.py
+++ main.py
@@ -50,6 +50,43 @@ def restartimage():
     lbl.configure(image=importedimage, width=380, height=320)
     lbl.image = importedimage
 
+def rotate(var):
+    image4 = Image.open(filename)
+    rotatedimage = image4.rotate(rotateimage.get())
+    rotatedimage = ImageTk.PhotoImage(rotatedimage)
+    lbl.configure(image=rotatedimage, width=380, height=320)
+    lbl.image = rotatedimage
+
+def changered(var):
+    image5 = Image.open(filename)
+    r, g, b = image5.split()
+    r = r.point(lambda i: redimage.get())
+    coloredimage = Image.merge("RGB", (r, g, b))
+    coloredimage.getextrema()
+    coloredimage = ImageTk.PhotoImage(coloredimage)
+    lbl.configure(image=coloredimage, width=380, height=320)
+    lbl.image = coloredimage
+
+def changeblue(var):
+    image5 = Image.open(filename)
+    r, g, b = image5.split()
+    b = b.point(lambda i: blueimage.get())
+    coloredimage = Image.merge("RGB", (r, g, b))
+    coloredimage.getextrema()
+    coloredimage = ImageTk.PhotoImage(coloredimage)
+    lbl.configure(image=coloredimage, width=380, height=320)
+    lbl.image = coloredimage
+
+def changegreen(var):
+    image5 = Image.open(filename)
+    r, g, b = image5.split()
+    g = g.point(lambda i: greenimage.get())
+    coloredimage = Image.merge("RGB", (r, g, b))
+    coloredimage.getextrema()
+    coloredimage = ImageTk.PhotoImage(coloredimage)
+    lbl.configure(image=coloredimage, width=380, height=320)
+    lbl.image = coloredimage
+
 # icon
 image_icon = ImageTk.PhotoImage(file="images/tulips.jpeg")
 window.iconphoto(False, image_icon)
@@ -87,5 +124,25 @@ Button(transformationsection, text="BLUR", width=12, height=2, font="arial 14 bo
 Button(transformationsection, text="CONTOUR", width=12, height=2, font="arial 14 bold", command=conturimage).place(x=155, y=50)
 Button(transformationsection, text="EMBOSS", width=12, height=2, font="arial 14 bold", command=embossimage).place(x=300, y=50)
 
+# rotate section
+Label(transformationsection, text="Rotate image:", font="arial 20 bold", fg="#fff", bg="#939f5c").place(x=10, y=100)
+
+rotateimage = Scale(transformationsection, from_=0, to=360, orient=HORIZONTAL, bg="#313715", length=420, command= rotate)
+rotateimage.place(x=10, y=140)
+
+# color change
+Label(transformationsection, text="Color change:", font="arial 20 bold", fg="#fff", bg="#939f5c").place(x=10, y=190)
+
+redimage = Scale(transformationsection, from_=0, to=255, orient=VERTICAL, bg="#313715", length=180, command=changered)
+redimage.place(x=40, y=230)
+Label(transformationsection, text="RED", font="arial 12 bold", fg="#fff", bg="#939f5c").place(x=40, y=420)
+
+greenimage = Scale(transformationsection, from_=0, to=255, orient=VERTICAL, bg="#313715", length=180, command=changegreen)
+greenimage.place(x=200, y=230)
+Label(transformationsection, text="GREEN", font="arial 12 bold", fg="#fff", bg="#939f5c").place(x=200, y=420)
+
+blueimage = Scale(transformationsection, from_=0, to=255, orient=VERTICAL, bg="#313715", length=180,  command=changeblue)
+blueimage.place(x=350, y=230)
+Label(transformationsection, text="BLUE", font="arial 12 bold", fg="#fff", bg="#939f5c").place(x=350, y=420)
 
 window.mainloop()
